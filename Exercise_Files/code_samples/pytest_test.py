d72510f782d9c2c99a849df637dcac8ffd16ef70
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-29 15:40:27 -0400

    Completed notes on all of section 2

---
+++ Exercise_Files/code_samples/pytest_test.py
@@ -0,0 +1,14 @@
+# def test_assert_true():
+#     """
+#     RED Stage: Hard code a failing test
+#     :return: Fail
+#     """
+#     assert False
+
+
+def test_assert_true():
+    """
+    GREEN Stage: Hard code a passing test
+    :return: Pass
+    """
+    assert True
