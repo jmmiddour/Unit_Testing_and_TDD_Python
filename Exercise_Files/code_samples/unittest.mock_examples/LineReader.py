7f2263db8f78fa1fa2c596f44cbbd601fd02057b
Joanne Middour <64432505+jmmiddour@users.noreply.github.com>
2021-05-31 15:55:00 -0400

    Completed chapter 5

--- Exercise_Files/code_samples/unittest.mock_examples/LineReader.py
+++ Exercise_Files/code_samples/unittest.mock_examples/LineReader.py
@@ -1,6 +1,12 @@
 import os
 
-def readFromFile(filename):
+
+def read_from_file(filename):
+    """
+    Function to read data in given file
+    :param filename: path to the file
+    :return:
+    """
     if not os.path.exists(filename):
         raise Exception("Bad File")
     infile = open(filename, "r")
